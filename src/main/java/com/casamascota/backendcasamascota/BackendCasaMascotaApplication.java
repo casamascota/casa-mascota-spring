90e981af04b68a44d1d725fd59e48dc917c86521
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-09-16 11:03:08 -0400

    first commit

---
+++ src/main/java/com/casamascota/backendcasamascota/BackendCasaMascotaApplication.java
@@ -0,0 +1,13 @@
+package com.casamascota.backendcasamascota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCasaMascotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCasaMascotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casamascota/backendcasamascota/BackendCasaMascotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casamascota.backendcasamascota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCasaMascotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
