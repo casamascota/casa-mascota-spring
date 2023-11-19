84fa98a72ab700001c485365c4211965768835de
CristorO <cristopher.olivera@ucb.edu.bo>
2023-11-19 10:54:48 -0400

    Fixed Persona backend

--- src/main/java/com/casamascota/backendcasamascota/entity/Persona.java
+++ src/main/java/com/casamascota/backendcasamascota/entity/Persona.java
@@ -1,86 +1,34 @@
 package com.casamascota.backendcasamascota.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "Persona")
 public class Persona {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_persona")
-    private Long idPersona;
-
-    @Column(name = "nombre")
+    private Long id_persona;
     private String nombre;
-
-    @Column(name = "apellido")
     private String apellido;
-
-    @Column(name = "numero_tel")
-    private String numeroTel;
-
-    @Column(name = "direccion")
+    private String numero_tel;
     private String direccion;
-
-    public Long getIdPersona() {
-        return idPersona;
-    }
-
-    public void setIdPersona(Long idPersona) {
-        this.idPersona = idPersona;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getNumeroTel() {
-        return numeroTel;
-    }
-
-    public void setNumeroTel(String numeroTel) {
-        this.numeroTel = numeroTel;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public Persona() {
-    }
-
-    public Persona(Long idPersona, String nombre, String apellido, String numeroTel, String direccion) {
-        this.idPersona = idPersona;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.numeroTel = numeroTel;
-        this.direccion = direccion;
-    }
 
     @Override
     public String toString() {
         return "Persona{" +
-                "idPersona=" + idPersona +
+                "idPersona=" + id_persona +
                 ", nombre='" + nombre + '\'' +
                 ", apellido='" + apellido + '\'' +
-                ", numeroTel='" + numeroTel + '\'' +
+                ", numeroTel='" + numero_tel + '\'' +
                 ", direccion='" + direccion + '\'' +
                 '}';
     }
