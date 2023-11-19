215af0ef1ed3eb3a647dc108afeafd4617bf89e4
CristorO <cristopher.olivera@ucb.edu.bo>
2023-11-19 11:18:37 -0400

    Usuario backend

---
+++ src/main/java/com/casamascota/backendcasamascota/entity/Usuario.java
@@ -0,0 +1,36 @@
+package com.casamascota.backendcasamascota.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Usuario")
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_usuario;
+    private String username;
+    private String hashed_password;
+    private String rol;
+
+    @ManyToOne
+    @JoinColumn(name = "id_persona")
+    private Persona persona;
+    private String especialidad;
+
+}
