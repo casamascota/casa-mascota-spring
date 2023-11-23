21ec401236955804f04934d0a0b636d2dfc1481d
Natalia-Gutierrez <bgutisol2019@gmail.com>
2023-11-23 17:08:39 -0400

    CAMBIOS

--- Frontend/src/backend/api/v1/routes/email_route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const emailController = require('../controllers/email_controller');
-
-router.post('/send', emailController.sendEmail);
-
-module.exports = router;
