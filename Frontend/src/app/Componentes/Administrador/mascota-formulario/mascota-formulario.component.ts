89dc4ec8e58b05fe972fded9542c798b3f685f2e
Natalia-Gutierrez <bgutisol2019@gmail.com>
2023-11-27 02:03:28 -0400

    modificaciones Cristopher 6724911

--- Frontend/src/app/Componentes/Administrador/mascota-formulario/mascota-formulario.component.ts
+++ Frontend/src/app/Componentes/Administrador/mascota-formulario/mascota-formulario.component.ts
@@ -1,21 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
 
 @Component({
   selector: 'app-mascota-formulario',
   templateUrl: './mascota-formulario.component.html',
-  styleUrls: ['./mascota-formulario.component.css']
+  styleUrls: ['./mascota-formulario.component.css'],
 })
 export class MascotaFormularioComponent {
   formMascota: FormGroup;
   genero: any = ['Macho', 'Hembra'];
   URL_BASE = 'http://localhost:3000/api/';
+  maxFechaNacimiento: string;
   // id_mascota: number = 0;
   // email: string = "";
   // mensaje: string = "";
 
   constructor(private httpClient: HttpClient, private formBuilder: FormBuilder ) { 
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1; // Los meses comienzan desde 0
+    const day = today.getDate();
+    this.maxFechaNacimiento = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+
     this.formMascota = this.formBuilder.group({
       nombre: ['',Validators.required],
       raza: ['',Validators.required],
